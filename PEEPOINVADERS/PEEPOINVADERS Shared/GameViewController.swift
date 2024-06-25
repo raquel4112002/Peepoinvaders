0aa177ed5fce5938cbcec8905293820587f59814
Gonçalo Gomes <101401818+Gominha123@users.noreply.github.com>
2024-06-25 17:42:52 +0100

    Update GameViewController.swift
    button e seize adjustment

--- PEEPOINVADERS/PEEPOINVADERS Shared/GameViewController.swift
+++ PEEPOINVADERS/PEEPOINVADERS Shared/GameViewController.swift
@@ -14,16 +14,36 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene()
-
-        // Present the scene
-        let skView = self.view as! SKView
-        skView.presentScene(scene)
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+             let scene = GameScene(size : view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
         
-        skView.ignoresSiblingOrder = true
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        let button = UIButton()
+        button.setTitle("Button", for: .normal)
+        button.backgroundColor = .systemRed
+        view.addSubview(button)
+        button.frame = CGRect(x: 100, y: 200, width: 100, height: 50)
+        button.layer.cornerRadius = 25
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        
     }
+    
+    @objc func buttonPressed() {
+        
+    }
+    
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
@@ -37,3 +57,4 @@ class GameViewController: UIViewController {
         return true
     }
 }
+
