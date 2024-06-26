d2a8f9e3cf5454e09189db1386a480e70517dbd9
Gonçalo Gomes <101401818+Gominha123@users.noreply.github.com>
2024-06-26 18:05:49 +0100

    Update GameViewController.swift

--- PEEPOINVADERS/PEEPOINVADERS Shared/GameViewController.swift
+++ PEEPOINVADERS/PEEPOINVADERS Shared/GameViewController.swift
@@ -11,12 +11,16 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    
+    var scene: GameScene!
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-             let scene = GameScene(size : view.bounds.size)
+                scene = GameScene(size : view.bounds.size)
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -30,18 +34,77 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
         
-        let button = UIButton()
-        button.setTitle("Button", for: .normal)
-        button.backgroundColor = .systemRed
-        view.addSubview(button)
-        button.frame = CGRect(x: 100, y: 200, width: 100, height: 50)
-        button.layer.cornerRadius = 25
-        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        let shootBtn = UIButton()
+        shootBtn.setTitle("Shoot", for: .normal)
+        shootBtn.backgroundColor = .systemRed
+        view.addSubview(shootBtn)
+        shootBtn.frame = CGRect(x: view.bounds.size.width - 100 , y: view.bounds.size.height - 100, width: 100, height: 50)
+        shootBtn.addTarget(self, action: #selector(ShootBullet), for: .touchUpInside)
+        
+        
+        let leftBtn = UIButton()
+        leftBtn.setTitle("Left", for: .normal)
+        leftBtn.backgroundColor = .systemRed
+        view.addSubview(leftBtn)
+        leftBtn.frame = CGRect(x: 25, y: view.bounds.size.height - 100, width: 50, height: 50)
+        
+        leftBtn.addTarget(self, action: #selector(MoveLeftStart), for: .touchDown)
+        leftBtn.addTarget(self, action: #selector(MoveLeftStop), for: .touchUpInside)
+        leftBtn.addTarget(self, action: #selector(MoveLeftStop), for: .touchUpOutside)
+        
+        
+        let rightBtn = UIButton()
+        rightBtn.setTitle("Right", for: .normal)
+        rightBtn.backgroundColor = .systemRed
+        view.addSubview(rightBtn)
+        rightBtn.frame = CGRect(x: 90, y: view.bounds.size.height - 100, width: 50, height: 50)
+        rightBtn.addTarget(self, action: #selector(MoveRightStart), for: .touchDown)
+        rightBtn.addTarget(self, action: #selector(MoveRightStop), for: .touchUpInside)
+        rightBtn.addTarget(self, action: #selector(MoveRightStop), for: .touchUpOutside)
+        
+        let special = UIButton()
+        special.setTitle("Special", for: .normal)
+        special.backgroundColor = .systemRed
+        view.addSubview(special)
+        special.frame = CGRect(x: view.bounds.size.width - 100 , y: view.bounds.size.height - 175, width: 100, height: 50)
+        special.addTarget(self, action: #selector(ShootSpecial), for: .touchUpInside)
         
     }
     
-    @objc func buttonPressed() {
-        
+    
+    
+    @objc func ShootBullet() {
+        scene.fireBullet()
+    }
+    
+    @objc func MoveLeftStart() {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(performMoveLeft), userInfo: nil, repeats: true)
+    }
+    
+    @objc func MoveLeftStop(){
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc func performMoveLeft(){
+        scene.MoveLeft()
+    }
+    
+    @objc func MoveRightStart() {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(performMoveRight), userInfo: nil, repeats: true)
+    }
+    
+    @objc func MoveRightStop(){
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @objc func performMoveRight(){
+        scene.MoveRight()
+    }
+    
+    @objc func ShootSpecial() {
+        scene.ShootSpecial()
     }
     
 
