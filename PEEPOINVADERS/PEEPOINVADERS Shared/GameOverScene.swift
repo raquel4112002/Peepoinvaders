da390bab6f062c190c30365b32a1a5b56b4f7116
raquel4112002 <68569885+raquel4112002@users.noreply.github.com>
2024-06-27 16:32:11 +0100

    Update GameOverScene.swift

--- PEEPOINVADERS/PEEPOINVADERS Shared/GameOverScene.swift
+++ PEEPOINVADERS/PEEPOINVADERS Shared/GameOverScene.swift
@@ -7,3 +7,37 @@
 
 import SpriteKit
 
+class GameOverScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "gameOverBackground")
+        background.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        background.zPosition = -1
+        self.addChild(background)
+        
+        let gameOverLabel = SKLabelNode(text: "Game Over")
+        gameOverLabel.fontName = "AmericanTypewriter-Bold"
+        gameOverLabel.fontSize = 45
+        gameOverLabel.fontColor = UIColor.white
+        gameOverLabel.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        self.addChild(gameOverLabel)
+        
+        let retryLabel = SKLabelNode(text: "Tap to Retry")
+        retryLabel.fontName = "AmericanTypewriter-Bold"
+        retryLabel.fontSize = 25
+        retryLabel.fontColor = UIColor.white
+        retryLabel.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2 - 100)
+        self.addChild(retryLabel)
+        
+        let wait = SKAction.wait(forDuration: 2.0)
+        let fadeIn = SKAction.fadeIn(withDuration: 1.0)
+        retryLabel.run(SKAction.sequence([wait, fadeIn]))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+        let gameScene = GameScene(size: self.size)
+        self.view?.presentScene(gameScene, transition: transition)
+    }
+}
+
